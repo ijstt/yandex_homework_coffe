adb9beb34540db301dd750650528c61db82e7d0e
ijstt <119423325+ijstt@users.noreply.github.com>
2023-11-28 00:44:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 from PyQt5.QtWidgets import QApplication, QMainWindow
 import sys
+from PyQt5.QtSql import QSqlDatabase, QSqlTableModel
+from PyQt5.QtWidgets import QMainWindow
+
+from design import Ui_MainWindow
 
 
-class Window(QMainWindow):
-    pass
+class Window(Ui_MainWindow, QMainWindow):
+    def __init__(self) -> None:
+        super().__init__()
+        self.initUI()
+
+    def initUI(self) -> None:
+        db: QSqlDatabase = QSqlDatabase.addDatabase('QSQLITE')
+        db.setDatabaseName('coffee.sqlite')
+        db.open()
+        model: QSqlDatabase = QSqlTableModel(self, db)
+        model.setTable('') 
+        model.select()
+        self.table.setModel(model)
 
 
 if __name__ == "__main__":
